827e06693917f92ca3a88ea2eff4ec6c9b8f965a
Lynn <homlean@gmail.com>
2014-04-22 14:17:27 +0800

    Update mass_insertion.rb

--- ruby/redis/mass_insertion.rb
+++ ruby/redis/mass_insertion.rb
@@ -11,12 +11,13 @@ def gen_redis_proto (cmd)
 end
 
 Dir.glob('data/*.txt') do | file |
+    tag = File.basename(file, '.*')
 
     File.foreach(file).with_index { |line, line_num|
         keyword = line.split("\t", 2)[0].gsub('+', ' ').strip
         values = line.split("\t", 2)[1].strip.split("\t", 2)[1]
 
-        STDOUT.write(gen_redis_proto(["RPUSH", "ms:#{keyword}"].concat values.split("\t")))
+        STDOUT.write(gen_redis_proto(["RPUSH", "#{tag}:#{keyword}"].concat values.split("\t")))
     }
 
 end
